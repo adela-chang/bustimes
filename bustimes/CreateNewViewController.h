995d109e4183b320aa0c9dee72692881b1a480da
Adela <a.bar.of.soap@gmail.com>
2013-09-28 15:01:08 -0700

    added createnew page

---
+++ bustimes/CreateNewViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateNewViewController.h
+//  bustimes
+//
+//  Created by Adela Chang on 2013/09/28.
+//  Copyright (c) 2013年 Adela Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateNewViewController : UIViewController
+
+@end
---
+++ bustimes/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  bustimes
+//
+//  Created by Adela Chang on 2013/09/25.
+//  Copyright (c) 2013年 Adela Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
